8f460f3cb8a37cd4a6ec46b1aa637ca3a11be221
Youwen Deng <489642071.com>
2022-06-08 13:42:54 +0800

    一个demo类

---
+++ JavaDemo/src/com/fengying/demo/Demo.java
@@ -0,0 +1,11 @@
+package com.fengying.demo;
+
+/**
+ * @author BJ
+ * @create 2022 - 2022/6/8 - 13:41
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("this is demo!");
+    }
+}
