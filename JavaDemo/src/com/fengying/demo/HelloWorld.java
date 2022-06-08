4e31d99acb4deb117e2d026cdc14c35af8289b17
Youwen Deng <489642071.com>
2022-06-08 13:33:43 +0800

    一个HelloWorld类

---
+++ JavaDemo/src/com/fengying/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.fengying.demo;
+
+/**
+ * @author BJ
+ * @create 2022 - 2022/6/8 - 13:31
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!");
+    }
+}
