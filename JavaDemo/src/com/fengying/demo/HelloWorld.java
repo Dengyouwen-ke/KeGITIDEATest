2d6912a1814b0720c9c89332c09b4e749f48a0f4
Dengyouwen-ke <51698798+Dengyouwen-ke@users.noreply.github.com>
2022-06-08 13:38:02 +0800

    Update HelloWorld.java

--- JavaDemo/src/com/fengying/demo/HelloWorld.java
+++ JavaDemo/src/com/fengying/demo/HelloWorld.java
@@ -7,5 +7,6 @@ package com.fengying.demo;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
+         System.out.println("Hello!");
     }
 }
